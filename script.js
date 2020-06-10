d018a1f582a840ed050b06ba5c89be2ed1978188
Jimmy B. Kazadi <binjim2002@yahoo.fr>
2020-06-10 12:51:05 -0700

    updated homework

--- script.js
+++ script.js
@@ -7,9 +7,22 @@ var UpperCaseLetters = [ "A", "B", "C", "D", "E", "F", "G","H", "I", "J", "K", "
 
 // Write password to the #password input
 //user need to specify the length and the type of Characters that they need to use
+var possibleCharacters = []
+ 
+  
 
-function getUserChoice() {
-  var length = prompt("how character do you want for your password?")
+ 
+function generatePassword() {
+  var length = prompt("how many character do you want for your password?")
+  if (length < 8 ){
+    alert("Your password is too short")
+return
+
+  }
+  if (length > 128 ){
+    alert("Your password is too long")
+    return 
+  }
   console.log(length)
 var hasLowercaseLetters = confirm("Do you want your password to have Lower case letters?")
   console.log(hasLowercaseLetters)
@@ -20,19 +33,34 @@ var hasLowercaseLetters = confirm("Do you want your password to have Lower case 
   var hasnumbers = confirm("Do you want your password to have numbers?")
   console.log(hasnumbers)
 
-  if (length < 8 ){
-    alert("Your password is too short")
-
-  }
-  if (length > 128 ){
-    alert("Your password is too long")
-    
+  
+  if ( hasLowercaseLetters) {
+    possibleCharacters = possibleCharacters.concat(lowercaseLetters)
   }
 
-} 
+  // 1. Add if conditions for hasUpperCaseLEtters
+if ( hasUpperCaseLetters) {
+  possibleCharacters = possibleCharacters.concat(UpperCaseLetters)
+}
+  // 2. Add if conditions for hasSpecialCharacters
+  if ( hasspecialCharacters) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters)
+  }
 
-getUserChoice()
+  
+var newPassword = "";
+ 
+  for (var i=0; i<length; i++){
+    // 4. Run this loop Each time, grab a random number.
+    // 5. Use that number to pick a character from the possibleCharacters array
+    // 6. Add it to a newPassword
+  }
+// 7. Return the new string here, instead of the hardcoded value "newPassword"
+  return "newPassword"
+}
+
 function writePassword() {
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
